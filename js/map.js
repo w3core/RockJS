67a529a19263501e11654c8aa37a0ebd920272a0
Max Chuhryaev <w3core@gmail.com>
2017-05-19 14:00:05 +0300

    Documentation changes

--- js/map.js
+++ js/map.js
@@ -1,1 +1,1 @@
-$R.page.define('doc').route('/doc(/*)').title('RockJS documentation').layout(null,'doc').module('header-area',null,null,'header').module('sidebar-area',null,null,'docMenu').module('content-area',null,null,'docContent').module('footer-area',null,null,'footer');
+$R.page.define('doc').route('/doc(/*)').title('RockJS documentation').layout(null,'doc').module('header-area',null,null,'docHeader').module('header-area',null,null,'docSearchResults').module('sidebar-area',null,null,'docMenu').module('content-area',null,null,'docContent').module('footer-area',null,null,'docFooter');
